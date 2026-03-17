5e8296d3cbd8907ef8d9aa613c370c0942a7a221
TheUnknownIdeal <apapartanen@gmail.com>
2026-03-17 13:44:58 +0200

    initialized driver class

--- max30102.h
+++ max30102.h
@@ -1,3 +1,25 @@
+#ifndef MAX30102_H
+#define MAX30102_H
+
+#include <Arduino.h> // Gives you access to byte, HIGH, LOW, etc.
+#include <Wire.h>
+
+
+class MAX30102 {
+    public:
+
+    private:
+        static const byte fifo_data = 0x07;
+        
+
+};
+
+
+
+Define
+
 REG_MODE_CONFIG = 0x09
 REG_FIFO_DATA   = 0x07
 REG_SPO2_CONFIG = 0x0A
+
+#endif
